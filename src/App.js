66094dc04be5652c38979dd0221a4194941e0e1c
Achi <achiosidze@gmail.com>
2023-05-15 23:40:00 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [loading, setLoading] = useState(false)
+  const [users, setUsers] = useState({})
+  const [value,setValue] = useState()
+
+  const api = async () => {
+    setLoading(true)
+    try {
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/todos/${value}`);
+      setUsers(response.data);
+      console.log(response.data);
+    } catch (error) {
+      alert(error?.message);
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    if(value > 3){
+     api()
+    }
+  },[value])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{loading? 'Loading...': loading}</p>
+      <p>Title: {users?.title}</p>
+      <p>ID: {users?.id}</p>
+      <input placeholder='enter' onChange={(e) => setValue(e.target.value)}/>
+      <button onClick={()=> api()}>Get Data</button>
     </div>
   );
 }
